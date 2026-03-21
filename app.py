3c9ddd6978ced35506fe89e2ccfcf6058da5451a
sooraj <infinitygamer1428@gmail.com>
2026-03-21 22:03:19 +0530

    First commit

--- app.py
+++ app.py
@@ -481,5 +481,6 @@ def init_db():
 
 # ---------------- MAIN ----------------
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
